9a7f7c05edb1ae5a70aebf0db7f495cc5960c427
Vladimir Perepelita <stenty2s@gmail.com>
2026-04-27 00:00:25 +0300

    Update .gitignore to include Playwright-related directories and update package-lock.json with new dependencies.

--- src/app/[locale]/tournaments/[tournament]/groups/[group]/ServerFetching.tsx
+++ src/app/[locale]/tournaments/[tournament]/groups/[group]/ServerFetching.tsx
@@ -1,23 +1,26 @@
-'use client'
-import GroupClient from '@/components/Group/GroupClient'
-import { useDeleteMatchMutation } from '@/hooks/mutations/useDeleteMatchMutation'
-import { useFetchGroupQuery } from '@/hooks/queries/useFetchGroupQuery'
-import { useFetchTournamentQuery } from '@/hooks/queries/useFetchTournamentQuery'
-import { UseMutationResult, useQueryClient } from '@tanstack/react-query'
-import { Params } from 'next/dist/server/request/params'
+'use client';
+import GroupClient from '@/components/Group/GroupClient';
+import { useDeleteMatchMutation } from '@/hooks/mutations/useDeleteMatchMutation';
+import { useFetchGroupQuery } from '@/hooks/queries/useFetchGroupQuery';
+import { useFetchTournamentQuery } from '@/hooks/queries/useFetchTournamentQuery';
+import { UseMutationResult, useQueryClient } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
+import { Params } from 'next/dist/server/request/params';
 
 function ServerFetching({ params }: { params: Params }) {
-	const { group, tournament } = params
-	const groupId = typeof group === 'string' ? group : String(group)
+	const { group, tournament } = params;
+	const groupId = typeof group === 'string' ? group : String(group);
 	const tournamentId =
-		typeof tournament === 'string' ? tournament : String(tournament)
-	const queryClient = useQueryClient()
-	const { data: groupData, isLoading, error } = useFetchGroupQuery(groupId)
-	const { data: tournamentData } = useFetchTournamentQuery(tournamentId)
-	const deleteMatchMutation = useDeleteMatchMutation(groupId, queryClient)
+		typeof tournament === 'string' ? tournament : String(tournament);
+	const queryClient = useQueryClient();
+	const { data: groupData, isLoading, error } = useFetchGroupQuery(groupId);
+	const { data: tournamentData } = useFetchTournamentQuery(tournamentId);
+	const deleteMatchMutation = useDeleteMatchMutation(groupId, queryClient);
+
+	const tTournament = useTranslations('TournamentGroup');
 
 	if (!tournamentData) {
-		return <div>Tournament not found</div>
+		return <div>{tTournament('group_not_found')}</div>;
 	}
 	return (
 		<GroupClient
@@ -31,7 +34,7 @@ function ServerFetching({ params }: { params: Params }) {
 			}
 			tournamentId={tournamentId}
 		/>
-	)
+	);
 }
 
-export default ServerFetching
+export default ServerFetching;
